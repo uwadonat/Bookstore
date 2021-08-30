4dbba696033f6f94c5b30e022c97a86610c10a62
uwadonat <uwadonat@gmail.com>
2021-08-30 17:09:50 +0200

    add Books function

---
+++ src/components/appComponents/Books.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BookList from './booksComponents/BookList';
+
+
+function books() {
+    return (
+        <div>
+            <BookList />
+        </div>
+    );
+}
+
+export default books;
