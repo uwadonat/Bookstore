86fbbb2603bc0f3cdba1ee537983d9fc8aff9e92
uwadonat <uwadonat@gmail.com>
2021-08-31 14:44:41 -0400

    Add Events

--- src/components/appComponents/booksComponents/AddBooForm.js
+++ src/components/appComponents/booksComponents/AddBooForm.js
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../../redux/books';
 
 function AddBookForm() {
+  const dispatch = useDispatch();
+  const [formState, setstate] = usestate({ title: '', author: '', category: '', });
+
+  function handleChange(e) {
+      settFormState({ ...formState, [e.target.name]: e.target.value});
+  }
+
+  function handleSubmit(e) {
+      dispatch(addBook(formState));
+      e.preventDefault();
+      setFormState({
+          ...formState, title: '', author: '', category: '',
+      });
+  }
+
   return (
     <div>
       <h2>Add NEW BOOK</h2>
-      <form>
-        <input type="text" name="title" placeholder="Book title" />
-        <input type="text" placeholder="Author" />
-        <select name="category" defaultValue="Category">
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="title" placeholder="Book title" value={formState.title} onChange={handleChange}/>
+        <input type="text" placeholder="Author" value={formState.author} onChange={handleChange}/>
+        <select name="category" defaultValue="Category" value={formState.category} onChange={handleChange}>
           <option value="">Category</option>
           <option value="">Sci-Fi</option>
           <option value="">Action</option>
