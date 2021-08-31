6a483bdb2134025a67312cd33a0a5c384587c965
uwadonat <uwadonat@gmail.com>
2021-08-31 11:10:14 -0400

    Add an input for Author

--- src/components/appComponents/booksComponents/AddBooForm.js
+++ src/components/appComponents/booksComponents/AddBooForm.js
@@ -3,10 +3,18 @@ import React from 'react';
 function AddBookForm() {
   return (
     <div>
-      <h2>Add  NEW BOOK</h2>
+      <h2>Add NEW BOOK</h2>
       <form>
         <input type="text" name="title" placeholder="Book title" />
-        <button type="button" className="add-btn btn-primary">ADD BOOK</button>
+        <input type="text" placeholder="Author" />
+        <select name="category" defaultValue="Category">
+          <option value="">Category</option>
+          <option value="">Sci-Fi</option>
+          <option value="">Action</option>
+        </select>
+        <button type="button" className="add-btn btn-primary">
+          ADD BOOK
+        </button>
       </form>
     </div>
   );
