3f3a70556f3ff5fde2f2a19bf0452bf73a2d6090
uwadonat <uwadonat@gmail.com>
2021-09-01 10:53:32 -0400

    refactor add_book and Remove_book actions

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,17 +1,21 @@
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
+const LOAD_BOOKS = 'bookstore/books/LOAD_BOOKS';
+const APP_ID = 'W2TAgTVlPqLEGcqZHoYR';
 
 const defaultState = [];
 
 export default function books(state = defaultState, action) {
   switch (action.type) {
-    case ADD_BOOK:
+    case LOAD_BOOKS:
+      return action.payload.map((bookObject) => ({...bookObject, author: 'Suzanne Collins', progress: 74 }));
+    case ADD_BOOKS:
       return state.concat({
-        id: Date.now(),
+        id: action.payload.item_id,
         title: action.payload.title,
-        author: action.payload.author,
+        author: 'suzanne Collins',
         category: action.payload.category,
-        progress: 0,
+        progress: 74,
       });
     case REMOVE_BOOK:
       return state.filter((book) => book.id !== action.payload);
@@ -20,16 +24,24 @@ export default function books(state = defaultState, action) {
   }
 }
 
-export function addBook(payload) {
+function loadbooks(payload) {
+  return {
+    type: LOAD_BOOKS,
+    payload,
+  };
+}
+
+function addBook(payload) {
   return {
     type: ADD_BOOK,
     payload,
   };
 }
 
-export function removeBook(payload) {
+function removeBook(payload) {
   return {
     type: REMOVE_BOOK,
     payload,
   };
 }
+
